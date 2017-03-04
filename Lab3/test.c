4b78938eb7a2f8273bbd76fbd0883e1d95c0df8d
Christopher Bradshaw <christopherbaxterbradshaw@gmail.com>
2017-03-04 13:51:16 -0600

    initial lab3 commit

---
+++ Lab3/test.c
@@ -0,0 +1,24 @@
+/* CSCI 4061 Assignment 3
+   Student ID: 
+   Student Name: 
+   test.c
+*/
+
+
+/* Test Helper Interface */
+void write_into_filesystem(char* input_directory, char *log_filename);
+void make_filesystem_summary(char* filename);
+void read_images_from_filesystem_and_write_to_output_directory(char* output_directory);
+void generate_html_file(char* filename);
+
+/* Main function */
+int main(int argc, char* argv[]){
+
+        // Command line arguments: <executable-name> <input_dir> <output_dir> <log_filename>
+
+        /*
+                Fill in the codes
+        */  	
+
+        return 0;
+}
